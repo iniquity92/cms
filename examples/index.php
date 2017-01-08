fc80689955074846eee282d27b5675e69624b98f
iniquity92 <sonu.abhisheksingh143@gmail.com>
2017-01-08 20:59:25 +0530

    Add files via upload

--- examples/index.php
+++ examples/index.php
@@ -6,26 +6,26 @@
     <head>
         <meta charset='utf-8' />
         <meta name='viewport' content='width=device-width,initial-scale=1' />
-        <link href='static/jqm/jquery.mobile-1.4.5.css' rel='stylesheet' type='text/css' />
+	<link href='static/jqm/jquery.mobile-1.4.5.css' rel='stylesheet' type='text/css' />
+	<link href='static/css/jqm-custom.css' rel='stylesheet' type='text/css' />
     </head>
     <body> 
-        <div data-role='page'>
-            <div data-role='header'>
+        <div id='data-role-pg1' data-role='page' class='jqm-demos'>
+            <div id='page-header' data-role='header'>
                 <h1>Title</h1>
             </div>        
-            <div id='pg1' role='main' class='ui-content'> 
-		        <div class='ui-grid-b'>
-                    <div class='ui-block-a' id='navigation'></div>
-                    <div class='ui-block-b' id='maincontent'>
+            <div id='pg1' role='main' class='ui-content jqm-content jqm-fullwidth'> 
+		      
+             
+                    <!--div class='full-width' id='maincontent'>
                                      
-                    </div>
-                    <div class='ui-block-c' id='blank'></div>
-		        </div>
+                    </div-->
+                    
             </div>
             <div data-role='footer'>Footer</div>
         </div>
         <script src='static/jquery.js'></script>
-        <script src='statec/jqm/jquery.mobile-1.4.5.js'></script>
+        <script src='static/jqm/jquery.mobile-1.4.5.js'></script>
 	<script src='static/create-dom.js'></script>
 	<script src='static/navigation.js'></script>
 	<script src='static/index.js'></script>
